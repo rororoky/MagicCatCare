d04bf40a495d5b5c52265d8f204e8316fdd8951f
rororoky <rororoky@users.noreply.github.com>
2017-08-15 22:55:07 +0800

    Add files via upload

---
+++ Assets/Script/CameraBehavior.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LittleCat
+{
+    public class CameraBehavior : MonoBehaviour
+    {
+        private float speed = 5f;
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            float positionX = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+            float positionY = Input.GetAxis("Vertical") * speed;
+
+            transform.SetPositionAndRotation(new Vector3(positionX, positionY, 0), new Quaternion(0, 0, 0, 0));
+        }
+    }
+}
---
+++ Assets/Script/CatTouch.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LittleCat
+{
+    public class CatTouch : MonoBehaviour
+    {
+
+        public Transform newFood;
+        // Use this for initialization
+        void Start()
+        {
+
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetButtonDown("Fire1"))
+            {
+                Transform food = Instantiate(newFood, transform.position, transform.rotation);
+
+                Vector3 fwd = transform.TransformDirection(Vector3.forward);
+
+                food.GetComponent<Rigidbody>().AddForce(500* fwd);
+            }
+        }
+    }
+}
